438d009e1d02a2464a404deeb299b02e418d915b
Bodempudi Naga Deepthi <nagadeepthibodempudi@gmail.com>
2026-03-11 21:14:13 +0530

    switch1.py

---
+++ LearnBasics/ThingsToKnow/switch1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def whichWeekDay(self, day):
+        if day >= 1 and day < 8:
+            match day:
+                case 1:
+                    d = 'Monday'
+                case 2:
+                    d = 'Tuesday'
+                case 3:
+                    d = 'Wednesday'
+                case 4:
+                    d = 'Thursday'
+                case 5:
+                    d = 'Friday'
+                case 6:
+                    d = 'Saturday'
+                case 7:
+                    d = 'Sunday'
+            print(d)
+        else:
+            print('Invalid')
